7bee1f5967691794524bdaf0dff7a9a913b5c018
marlil <marlil@stud.ntnu.no>
2018-09-10 11:16:12 +0200

    Begynt med å sjekke om tallet er i spill allerede

---
+++ src/no/ntnu/imt3281/sudoku/sudokuController.java
@@ -0,0 +1,22 @@
+package no.ntnu.imt3281.sudoku;
+
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+
+public class sudokuController {
+
+    @FXML
+    private Button newGame;
+    
+    @FXML
+    private Canvas canvas;
+    
+    @FXML
+    void newGame(ActionEvent event) {
+
+    }
+
+}
