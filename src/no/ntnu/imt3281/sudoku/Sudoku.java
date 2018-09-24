57aa52dc39f18f4ba2872cd9e3a1c5c89e5903be
Marius <marlil@stud.ntnu.no>
2018-09-24 15:33:38 +0200

    Gui er pen og kan kun skrive gyldig input i brett

--- src/no/ntnu/imt3281/sudoku/Sudoku.java
+++ src/no/ntnu/imt3281/sudoku/Sudoku.java
@@ -11,76 +11,15 @@ import javafx.stage.Stage;
 public class Sudoku extends Application {
 	
 	public static void main(String args[]) {
-		System.out.println(" text1 " );
 		launch(args);
-		System.out.println(" text3 " );
 	}
 	
 	@Override
-	public void start(Stage stage) throws IOException {
+	public void start(Stage primaryStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
 		Scene scene = new Scene(root);
-		stage.setTitle("Sudoku");
-		stage.setScene(scene);
-		stage.show();
+		primaryStage.setTitle("Sudoku");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
-	private int[][] originalBoard = new int[9][9];
-	private int[][] board = new int[9][9];
-	private static int selectedRow;
-	private static int selectedColumn;
-	
-	public static boolean checkValid(int row, int column, int[][] board, int number) {
-		for (int r = 0;r < 9;r++) {
-			if (number == board[row][r]) {
-				return false;
-			}
-		}
-
-		for (int c = 0;c < 9;c++) {
-			if (number == board[c][column]) {
-				return false;
-			}
-		}
-		
-		int block_x = (selectedRow/3)*3;
-		int block_y = (selectedColumn/3)*3;
-		
-		for (int n = 0;n < 3;n++) {
-			for (int m = 0;m < 3;m++) {
-				if (number == board[block_x+1][block_y+1]) {
-					return false;
-				}
-			}
-		}
-		
-		return true;
-	}
-	
-	
-	public static void insert(int number) {
-		
-	}
-	
-	public static void newGame() {
-		
-	}
-	
-	public static void flipV() {
-		
-	}
-	
-	public static void flipH() {
-		
-	}
-	
-	public static void flipDLR() {
-		
-	}
-	
-	public static void flipDRL() {
-		
-	}
-	
-
 }
--- src/no/ntnu/imt3281/sudoku/sudokuController.java
+++ src/no/ntnu/imt3281/sudoku/sudokuController.java
@@ -1,41 +1,113 @@
 package no.ntnu.imt3281.sudoku;
 
-
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class sudokuController {
 
-    @FXML
-    private BorderPane borderPane;
+	private static final int NUMB_COLS = 9;
+	private static final int NUMB_ROWS = 9;
+	private TextField[][] textFields;
 
-    @FXML
-    private Button NewGame;
-    
-    @FXML
-    private ToolBar ToolBar;
+	@FXML
+	private BorderPane borderPane;
 
-    @FXML
-    void newGame(ActionEvent event) {
-    	int rows = 9;
-    	int cols= 9;
-    	GridPane grid = new GridPane();
-    	TextField textFields[][] = new TextField[rows][cols];
-    	for (int r = 0; r<rows; r++) {
-    		for (int c = 0; c<cols; c++) {
-    			grid.add(textFields[r][c] = new TextField(), c, r);
-    		}
-    	}
-    	borderPane.setCenter(grid);
-    	grid.setGridLinesVisible(true);
-    	GridPane.setRowSpan(grid, 9);
-    	GridPane.setColumnSpan(grid, 9);
-    }
+	@FXML
+	private Button NewGame;
 
+	@FXML
+	private ToolBar ToolBar;
+
+	public sudokuController() {
+		textFields = new TextField[NUMB_ROWS][NUMB_COLS];
+		Platform.runLater(() -> {
+			generate();
+		});
+	}
+
+	public void generate() {
+		GridPane grid = new GridPane();
+		for (int r = 0; r < NUMB_ROWS; r++) {
+			for (int c = 0; c < NUMB_COLS; c++) {
+				TextField field = new TextField();
+				field.setPrefSize((borderPane.getHeight()) / NUMB_ROWS, (borderPane.getWidth()) / NUMB_COLS);
+				textFields[r][c] = field;
+				grid.add(field, c, r);
+				textFields[r][c].setAlignment(Pos.CENTER);
+				textFields[r][c].setFont(Font.font("system regular", FontWeight.BOLD, 25));
+				final int row = r;
+				final int col = c;
+				textFields[r][c].textProperty().addListener(new ChangeListener<String>() {
+					@Override
+					public void changed(ObservableValue<? extends String> observable, String oldValue,
+							String newValue) {
+						if (!newValue.matches("\\d*")) { // force the field to be numeric only
+							textFields[row][col].setText(newValue.replaceAll("[^\\d]", ""));
+						}
+						if (textFields[row][col].getText().length() > 1) {
+							textFields[row][col].setText(textFields[row][col].getText().substring(0, 1));
+						}
+						if (newValue.matches("0")) {
+							textFields[row][col].clear();
+						}
+						if (!checkValid(row, col, textFields, newValue)) {
+							textFields[row][col].clear();
+						}
+					}
+				});
+			}
+		}
+		borderPane.setCenter(grid);
+		grid.setGridLinesVisible(true);
+	}
+
+	@FXML
+	void newGame(ActionEvent event) {
+
+	}
+
+	public static boolean checkValid(int row, int column, TextField[][] board, String number) {
+		for (int r = 0; r < 9; r++) {
+			if (r != column) {
+				if (board[row][r].getText().equals(number)) {
+					return false;
+				}
+			}
+		}
+
+		for (int c = 0; c < 9; c++) {
+			if (c != row) {
+				if (board[c][column].getText().equals(number)) {
+					return false;
+				}
+			}
+		}
+
+		int rowStart = (row / 3) * 3;
+		int colStart = (column / 3) * 3;
+		int rowEnd = rowStart + 3;
+		int colEnd = colStart + 3;
+		for (int r = rowStart; r < rowEnd; r++) {
+			for (int c = colStart; c < colEnd; c++) {
+				if (r != row && c != column) {
+					if (board[r][c].getText().equals(number)) {
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
+	}
 }
